4ee5cd6f2f09494454960a4bcff598438e715e82
KwameGH22 <adarkwa.y.k@gmail.com>
2024-04-17 17:36:26 +0000

    header component added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { BsMoon } from "react-icons/bs";
+import { IoSunnyOutline } from "react-icons/io5";
+import { useAppContext } from "../../Context/ContextProvider";
 
 const Header = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { isDarkMode, ToggleDarkMode } = useAppContext();
 
-export default Header
+  return (
+    <header className="header">
+      <div className="headerContent">
+        <h1>Where in the world?</h1>
+        <div className="headerRight" onClick={ToggleDarkMode}>
+          {isDarkMode ? (
+            <IoSunnyOutline className="moon" />
+          ) : (
+            <BsMoon className="moon" />
+          )}
+          <h3>{isDarkMode ? "Light Mode" : "Dark Mode"}</h3>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
